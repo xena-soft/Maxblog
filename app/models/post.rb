02fe5f98dfe33a71619e062df8b883cdb6a36778
Maxim Ageev <maxsim.ageev@gmail.com>
2017-02-17 18:27:18 +0300

    fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
 
-  # default_value_for(:published_at) { Date.today}
+  default_value_for(:published_at) { Date.today}
 
   def author_nickname
     author.nickname
