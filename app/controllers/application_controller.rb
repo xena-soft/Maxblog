c113f44003ff146e9982530eb40d4e7ed48efb20
Maxim Ageev <maxsim.ageev@gmail.com>
2017-02-21 11:19:29 +0300

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :null_session
 
-  # before_action :authenticate_request
+  before_action :authenticate_request
 
   attr_reader :current_user
 
