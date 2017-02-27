875480308d8f19c8aab8c2216eeeed714d5526b8
Maxim Ageev <maxsim.ageev@gmail.com>
2017-02-27 10:28:49 +0300

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217091630) do
+ActiveRecord::Schema.define(version: 20170222165040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170217091630) do
     t.date     "published_at"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.index ["author_id"], name: "index_posts_on_author_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
